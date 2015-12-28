48fb5caa28b20fcaff3f8cf83c7443e090d8a5de
GoblinBlaster <og280343@gmail.com>
2015-12-28 16:30:31 +0900

    3e

--- app/src/main/java/com/example/estrella/days/MainActivity.java
+++ app/src/main/java/com/example/estrella/days/MainActivity.java
@@ -7,22 +7,29 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.GregorianCalendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     SharedPreferences preferences;
     SharedPreferences.Editor editor;
-    TextView tv_selDate  = (TextView)findViewById(R.id.tv_sel_date);
-    static int Y, M, D;
+    TextView tv_selDate;
+    Button btn_ok;
+    ImageView[] iviews;
+
+    static final int[] iv_id = {1,2};
+    static int Y, M, D, MM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv_selDate  = (TextView)findViewById(R.id.tv_sel_date);
 
         preferences = getSharedPreferences("DatePrefs", 0);
 
@@ -38,15 +45,32 @@ public class MainActivity extends AppCompatActivity {
         }
 
 //        tv_selDate = (TextView)findViewById(R.id.tv_sel_date);
-        String x = Y + "-" + M + "-"  + D;
+        MM = M+1;
+        String x = Y + "-" + MM + "-"  + D;
         tv_selDate.setText(x);
 
-        findViewById(R.id.btn_set_date).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+//        findViewById(R.id.btn_set_date).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                new DatePickerDialog(MainActivity.this, dateSetListener, Y, M, D).show();
+//            }
+//        });
+
+        btn_ok = (Button)findViewById(R.id.btn_set_date);
+        btn_ok.setOnClickListener(this);
+        for(int i=0 ; i<2 ; i++){   //sizeOf
+            iviews[i] = (ImageView)findViewById(iv_id[i]);
+            iviews[i].setOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_set_date:
                 new DatePickerDialog(MainActivity.this, dateSetListener, Y, M, D).show();
-            }
-        });
+                break;
+        }
     }
 
     private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
